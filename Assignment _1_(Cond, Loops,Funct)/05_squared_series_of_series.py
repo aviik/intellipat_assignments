cabfd239cf78c19c1d62502dfa57e36d693a5d1a
aviik <comeTOavik@gmail.com>
2017-10-12 22:29:39 +0530

    Assignment

--- Assignment _1_(Cond, Loops,Funct)/05_squared_series_of_series.py
+++ Assignment _1_(Cond, Loops,Funct)/05_squared_series_of_series.py
@@ -1,9 +1,14 @@
 ## 1^2 + ( 1^2 + 2^2 ) + (1^2 + 2^2 + 3^2) + .......+nth_number
-
+print("Give the nth number: ")
 nth_number = int(input("> "))
-total = 0
+
 i = 1
-for j in range(1, nth_number + 1):
-    total = j**2 + total
-    ##total = (j+1)**2
-print(total)
+j = 1
+sum = 0
+while i <= nth_number:
+    total = 0
+    for j in range(0,i+1):
+        total = total + j**2
+    sum = sum + total
+    i = i + 1
+print(sum)
